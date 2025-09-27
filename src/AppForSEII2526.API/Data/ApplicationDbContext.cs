85357db7474ef7682f48edc5ea5a0037684d2a7f
DANIEL MARTÍNEZ BAUTISTA <daniel.martinez28@alu.uclm.es>
2025-09-27 11:43:46 +0200

    actualizado (pero comentado) ApplicationDBContext, BonosComprados puede no estar bien

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,4 +5,17 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    /*
+    public DbSet<BonoBocadillo> BonoBocadillos { get; set; }
+    public DbSet<BonosComprados> BonosComprados { get; set; }
+    public DbSet<CompraBono> ComprasBono { get; set; }
+    public DbSet<TipoBocadillo> TiposBocadillos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<BonosComprados>().HasKey(bc => new { bc.bonoId, bc.compraId });
+        builder.Entity<BonoBocadillo>().HasKey(bb => new { bb.bonoID });
+    }
+    */
 }
