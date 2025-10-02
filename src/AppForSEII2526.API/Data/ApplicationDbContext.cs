986820959a1c928809078dbedc2ec3d40bcea132
rafamartinez21 <Rafael.Martinez18@alu.uclm.es>
2025-10-02 18:18:50 +0200

    11 crear base de datos comprar merch del restaurante (#17)
    * Creacion Base de datos de comprar merch del restaurante y diagrama de clases.
    
    * He actualizado la clase ApplicationDbContext.

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,4 +5,9 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<Producto> Productos { get; set; }
+    public DbSet<TipoProducto> TiposProductos { get; set; }
+    public DbSet<Compra_Producto> Compras { get; set; }
+    public DbSet<Producto_Compra> Productos_Compras { get; set; }
+    
 }
