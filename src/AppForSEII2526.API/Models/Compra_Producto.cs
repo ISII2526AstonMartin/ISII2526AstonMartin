e58d27eeca0a22660af1b1250a8ffe44d66ee0d2
rafamartinez21 <Rafael.Martinez18@alu.uclm.es>
2025-10-03 10:23:31 +0200

    18 resolución de errores y modificación reglas de validación clases (#19)
    * He aplicado las reglas de validación.
    
    * He añadido la enumeración de método de pago.
    
    * He cambiado el tipo de dato del atributo Metodo_Pago

--- src/AppForSEII2526.API/Models/Compra_Producto.cs
+++ src/AppForSEII2526.API/Models/Compra_Producto.cs
@@ -3,16 +3,27 @@
     public class Compra_Producto
     {
         // Atributos
+        [Required]
         public string Apellido1 { get; set; }
         public string Apellido2 { get; set; }
         [Key]
         public string CompraID { get; set; }
+        [Required]
         public string Direccion_Envio { get; set; }
         public DateTime FechaCompra { get; set; }
-        public string Metodo_Pago { get; set; }
+        [Required]
+        public MetodoPago Metodo_Pago { get; set; }
+        [Required]
         public string Nombre { get; set; }
         public float PrecioFinal { get; set; }
         //Relaciones
         public List<Producto_Compra> Productos_Compras { get; set; }
     }
+    public enum MetodoPago
+    {
+        Tarjeta_Credito,
+        PayPal,
+        Gpay
+    }
+
 }
--- src/AppForSEII2526.API/Models/Producto.cs
+++ src/AppForSEII2526.API/Models/Producto.cs
@@ -3,10 +3,13 @@
     public class Producto
     {
         // Atributos
+        [Required]
         public string Nombre { get; set; }
         [Key]
         public string ProductoID { get; set; }
+        [Required]
         public float PVP { get; set; }
+        [Required]
         public int Stock { get; set; }
         //Relaciones
         public TipoProducto Tipo_Producto { get; set; }
