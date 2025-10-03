3879b119e37916004a43308031ae6b98fe8cb3c7
MarBauAB <Daniel.Martinez28@alu.uclm.es>
2025-10-03 14:44:45 +0200

    7 crear base de datos de bonos (#20)
    * Creada base de la base de datos (falta meterla en el applicationdbcontext)
    
    * actualizado (pero comentado) ApplicationDBContext, BonosComprados puede no estar bien
    
    * arreglada clase bonoscomprados.cs
    
    * arreglado nullable en apellido2 de comprabono
    
    * arreglado mal tipo en comprabono
    
    * Clases completadas, bases de datos y migraciones completables, creación del diagrama
    
    * Solucionado mal tipo en TipoBocadillo

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -10,4 +10,15 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<Compra_Producto> Compras { get; set; }
     public DbSet<Producto_Compra> Productos_Compras { get; set; }
     
+    
+    public DbSet<BonoBocadillo> BonoBocadillos { get; set; }
+    public DbSet<BonosComprados> BonosComprados { get; set; }
+    public DbSet<CompraBono> ComprasBono { get; set; }
+    public DbSet<TipoBocadillo> TiposBocadillos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<BonosComprados>().HasKey(bc => new { bc.BonoId, bc.CompraId });
+    }
 }
