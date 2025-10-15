331a71a962a6fd7a959560f987ea9708b3033ea6
Antonio22-12 <Antonio.Garcia47@alu.uclm.es>
2025-10-15 17:38:42 +0200

    Creado el controlador basico de Get Bocadillos (#51)
    Creado el controlador basico de Get Bocadillos (#51)

---
+++ src/AppForSEII2526.API/Controllers/BocadillosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BocadillosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<BocadillosController> _logger;
+
+        public BocadillosController(ApplicationDbContext context, ILogger<BocadillosController> logger)
+        {
+            this._context = context;
+            this._logger = logger;
+        }
+       
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(List<Bocadillo>), (int)HttpStatusCode.OK)]
+            public async Task<IActionResult> GetBocadillosParaPedir()
+        {
+            IList<Bocadillo > bocadillos = await _context.Bocadillo
+                .ToListAsync();
+                return Ok(bocadillos);  
+        }
+
+    }
+}
