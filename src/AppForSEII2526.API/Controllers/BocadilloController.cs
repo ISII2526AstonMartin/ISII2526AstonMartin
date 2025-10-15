23120fe7e89127f6fa1c49581a3baab88dc77d50
Angel Lopez Hortelano <Angel.Lopez30@alu.uclm.es>
2025-10-15 16:58:05 +0200

    Crear metodo getBocadillosforResenya

--- src/AppForSEII2526.API/Controllers/BocadilloController.cs
+++ src/AppForSEII2526.API/Controllers/BocadilloController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppForSEII2526.API.Controllers
 {
@@ -7,5 +8,24 @@ namespace AppForSEII2526.API.Controllers
     [ApiController]
     public class BocadilloController : ControllerBase
     {
+
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<BocadilloController> _logger;
+        public BocadilloController(ApplicationDbContext context, ILogger<BocadilloController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<Bocadillo>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetBocadillosforResenya()
+        {
+            IList<Bocadillo> bocadillos = await _context.Bocadillo
+                .ToListAsync();
+            return Ok(bocadillos);
+        }
     }
 }
