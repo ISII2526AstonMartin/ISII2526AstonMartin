a23ca38f5dc3e3b703e3c84d689cbafee56055dc
Angel Lopez Hortelano <Angel.Lopez30@alu.uclm.es>
2025-10-20 18:24:52 +0200

    Crear DTOs basicos

---
+++ src/AppForSEII2526.API/DTOs/BocadillosParaResenyaDTO.cs
@@ -0,0 +1,38 @@
+﻿
+namespace AppForSEII2526.API.DTOs
+{
+    public class BocadillosParaResenyaDTO
+    {
+        public BocadillosParaResenyaDTO(int id, string nombre, float PVP, Tamanyo tamanyo, TipoPan tipoPan)
+        {
+            Id = id;
+            Nombre = nombre;
+            this.PVP = PVP;
+            Tamanyo = tamanyo;
+            TipoPan = tipoPan;
+
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public float PVP { get; set; }
+        public Tamanyo Tamanyo { get; set; }
+        public TipoPan TipoPan { get; set; }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is BocadillosParaResenyaDTO dTO &&
+                   Id == dTO.Id &&
+                   Nombre == dTO.Nombre &&
+                   PVP == dTO.PVP &&
+                   Tamanyo == dTO.Tamanyo &&
+                   EqualityComparer<TipoPan>.Default.Equals(TipoPan, dTO.TipoPan);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Nombre, PVP, Tamanyo, TipoPan);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/ResenyaDTO.cs
@@ -0,0 +1,70 @@
+﻿
+namespace AppForSEII2526.API.DTOs
+{
+    public class ResenyaDTO
+    {
+        public ResenyaDTO()
+        {
+            bocadillos = new List<BocadillosPuntuacion>();
+        }
+
+        public ResenyaDTO(int id, string? nombreUsuario, string titulo, string descripcion, ValoracionGeneral valoracionGeneral, List<BocadillosPuntuacion> bocadillos)
+        {
+            Id = id;
+            this.nombreUsuario = nombreUsuario;
+            this.titulo = titulo;
+            this.descripcion = descripcion;
+            this.valoracionGeneral = valoracionGeneral;
+            this.bocadillos = bocadillos;
+        }
+
+        public int Id { get; set; }
+        public string? nombreUsuario { get; set; }
+        [Required]
+        public string titulo { get; set; }
+        [Required]
+        public string descripcion { get; set; }
+        [Required]
+        public ValoracionGeneral valoracionGeneral { get; set; }
+
+
+        public enum ValoracionGeneral
+        {
+            Uno,
+            Dos,
+            Tres,
+            Cuatro,
+            Cinco
+        }
+
+
+        public List<BocadillosPuntuacion> bocadillos { get; set; }
+        public class BocadillosPuntuacion
+        {
+            public BocadillosPuntuacion(int idBocadillo, int puntuacion)
+            {
+                this.idBocadillo = idBocadillo;
+                this.puntuacion = puntuacion;
+            }
+            public int idBocadillo { get; set; }
+            public int puntuacion { get; set; }
+            
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ResenyaDTO dTO &&
+                   Id == dTO.Id &&
+                   nombreUsuario == dTO.nombreUsuario &&
+                   titulo == dTO.titulo &&
+                   descripcion == dTO.descripcion &&
+                   valoracionGeneral == dTO.valoracionGeneral &&
+                   EqualityComparer<List<BocadillosPuntuacion>>.Default.Equals(bocadillos, dTO.bocadillos);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, nombreUsuario, titulo, descripcion, valoracionGeneral, bocadillos);
+        }
+    }
+}
