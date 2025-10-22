41d6fbd90b361966c67d574aaaae6474ef84d9ad
DANIEL MARTÍNEZ BAUTISTA <daniel.martinez28@alu.uclm.es>
2025-10-22 16:14:18 +0200

    creado BonosController

---
+++ src/AppForSEII2526.API/Controllers/BonosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppForSEII2526.API.Controllers
+{
+    public class BonosController
+    {
+
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<BocadillosController> _logger;
+        public BonosController(ApplicationDbContext context, ILogger<BocadillosController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(List<Bocadillo>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetBonos()
+        {
+            IList<BonoBocadillo> bonobocadillos = await _context.BonoBocadillos
+                .ToListAsync();
+            return Ok(bonobocadillos);
+        }
+    }
+}
