75122bbe002df19f2db43ea192d846c962e238af
Angel Lopez Hortelano <Angel.Lopez30@alu.uclm.es>
2025-10-27 19:20:02 +0100

    Añadir DTOs necesarios para el POST

--- src/AppForSEII2526.API/DTOs/BocadillosDTO.cs
+++ src/AppForSEII2526.API/DTOs/BocadillosDTO.cs
@@ -8,6 +8,7 @@ namespace AppForSEII2526.API.DTOs
         public Tamanyo Tamanyo { get; set; }
         public string TipoPan { get; set; }
         public float PVP { get; set; }
+        public int Id { get; set; }
 
         public BocadillosDTO()
         {
@@ -16,6 +17,7 @@ namespace AppForSEII2526.API.DTOs
 
         public BocadillosDTO(int id, string nombre, Tamanyo tamanyo, string tipoPan, float PVP)
         {
+            Id = id;
             Nombre = nombre;
             Tamanyo = tamanyo;
             TipoPan = tipoPan;
@@ -26,6 +28,7 @@ namespace AppForSEII2526.API.DTOs
         public override bool Equals(object? obj)
         {
             return obj is BocadillosDTO dTO &&
+                   Id == dTO.Id &&
                    Nombre == dTO.Nombre &&
                    Tamanyo == dTO.Tamanyo &&
                    TipoPan == dTO.TipoPan &&
@@ -34,7 +37,7 @@ namespace AppForSEII2526.API.DTOs
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(Nombre, Tamanyo, TipoPan, PVP);
+            return HashCode.Combine(Id, Nombre, Tamanyo, TipoPan, PVP);
         }
     }
 }
--- src/AppForSEII2526.API/Models/TipoPan.cs
+++ src/AppForSEII2526.API/Models/TipoPan.cs
@@ -1,4 +1,5 @@
-﻿namespace AppForSEII2526.API.Models
+﻿
+namespace AppForSEII2526.API.Models
 {
     public class TipoPan
     {
@@ -7,5 +8,20 @@
         public List<Bocadillo> Bocadillos { get; set; }
         public string Nombre { get; set; }
 
+        public TipoPan(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is TipoPan pan &&
+                   Id == pan.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
     }
 }
