9727fb36e847a56447adc16282e94f6ed19871ad
DANIEL MARTÍNEZ BAUTISTA <daniel.martinez28@alu.uclm.es>
2025-10-27 13:09:45 +0100

    creada base de comprabonodto

---
+++ src/AppForSEII2526.API/DTOs/CompraBonoDTO.cs
@@ -0,0 +1,22 @@
+﻿
+namespace AppForSEII2526.API.DTOs
+{
+    public class CompraBonoDTO
+    {
+        public CompraBonoDTO() { }
+
+        public string CompraBonoId { get; set; }
+
+        public DateTime FechaCompraBono { get; set; }
+
+        public int NBono { get; set; }
+
+        public float PrecioTotalBono { get; set; }
+
+        public MetodoPago MetodoPagoUsuario { get; set; }
+
+        public ApplicationUser applicationuser { get; set; }
+
+        
+    }
+}
