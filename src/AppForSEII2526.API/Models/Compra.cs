36c29ec134b62fcf600aaef1436357be2c978b42
ANTONIO GARCÍA DE LA REINA AGUILAR <Antonio.Garcia47@alu.uclm.es>
2025-10-28 19:19:43 +0100

    Se ha puesto el constructor de la clase Compra

--- src/AppForSEII2526.API/Models/Compra.cs
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -2,7 +2,16 @@
 {
     public class Compra
     {
-        
+        public Compra(DateTime fechaCompra, List<CompraBocadillo> compraBocadillos, MetodoPago metodoPago, ApplicationUser usuario)
+        {
+
+            FechaCompra = fechaCompra;
+
+            CompraBocadillos = compraBocadillos;
+
+            MetodoPago = metodoPago;
+            this.usuario = usuario;
+        }
         public int CompraID { get; set; }
         [Required]
         public DateTime FechaCompra {  get; set; }
