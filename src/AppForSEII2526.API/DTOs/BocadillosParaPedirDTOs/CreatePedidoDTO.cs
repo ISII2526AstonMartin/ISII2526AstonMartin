7dd844c8d90b45cab7ac2f7f70530568293f5aa1
ANTONIO GARCÍA DE LA REINA AGUILAR <Antonio.Garcia47@alu.uclm.es>
2025-10-28 09:54:10 +0100

    Se ha creado y terminado el DTO de Pedido y se ha hecho el DTO de ItemPedido

--- src/AppForSEII2526.API/DTOs/BocadillosParaPedirDTOs/CreatePedidoDTO.cs
+++ src/AppForSEII2526.API/DTOs/BocadillosParaPedirDTOs/CreatePedidoDTO.cs
@@ -18,17 +18,21 @@ namespace AppForSEII2526.API.DTOs.BocadillosParaPedirDTOs
 
         public string? Apellido2 { get; set; }
 
+        public IList<ItemPedidoDTO> ItemPedido { get; set; }
 
-        public CreatePedidoDTO(string nombre, MetodoPago metododepago, string apellido1, string apellido2)
+
+        public CreatePedidoDTO(string nombre, MetodoPago metododepago, string apellido1, string apellido2, IList<ItemPedidoDTO> itempedido)
         {
             Nombre = nombre;
             MetodoPago = metododepago;
             Apellido1 = apellido1;
             Apellido2 = apellido2;
+            ItemPedido = itempedido;
+        }
 
-
-
-
+        public CreatePedidoDTO()
+        {
+            ItemPedido = new List<ItemPedidoDTO>();
         }
 
         public override bool Equals(object? obj)
@@ -37,12 +41,13 @@ namespace AppForSEII2526.API.DTOs.BocadillosParaPedirDTOs
                    Nombre == dTO.Nombre &&
                    MetodoPago == dTO.MetodoPago &&
                    Apellido1 == dTO.Apellido1 &&
-                   Apellido2 == dTO.Apellido2;
+                   Apellido2 == dTO.Apellido2 &&
+                  ItemPedido.SequenceEqual(dTO.ItemPedido);
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(Nombre, MetodoPago, Apellido1, Apellido2);
+            return HashCode.Combine(Nombre, MetodoPago, Apellido1, Apellido2, ItemPedido);
         }
     }
 }
---
+++ src/AppForSEII2526.API/DTOs/BocadillosParaPedirDTOs/ItemPedidoDTO.cs
@@ -0,0 +1,36 @@
+﻿namespace AppForSEII2526.API.DTOs.BocadillosParaPedirDTOs
+{
+    public class ItemPedidoDTO
+    {
+
+        public string nombreBocadillo { get; set; }
+
+        [Required]
+        public int Cantidad { get; set; }
+        public int PVP { get; set; }
+        public string TipoPan { get; set; }
+
+        public ItemPedidoDTO(string nombreBocadillo, int cantidad, int pVP, string tipoPan)
+        {
+            this.nombreBocadillo = nombreBocadillo;
+            Cantidad = cantidad;
+            PVP = pVP;
+            TipoPan = tipoPan;
+        }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ItemPedidoDTO dTO &&
+                   nombreBocadillo == dTO.nombreBocadillo &&
+                   Cantidad == dTO.Cantidad &&
+                   PVP == dTO.PVP &&
+                   TipoPan == dTO.TipoPan;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(nombreBocadillo, Cantidad, PVP, TipoPan);
+        }
+
+    }
+}
