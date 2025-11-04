66c606d2c3748e3f39d8d6338317907cd25afdf0
Angel Lopez Hortelano <Angel.Lopez30@alu.uclm.es>
2025-11-04 12:40:39 +0100

    Creadas Pruebas para GetDetails

--- src/AppForSEII2526.API/Models/Bocadillo.cs
+++ src/AppForSEII2526.API/Models/Bocadillo.cs
@@ -4,7 +4,19 @@ namespace AppForSEII2526.API.Models
 {
     public class Bocadillo
     {
-        
+
+        public Bocadillo(int id, string nombre, float pVP, int stock, Tamanyo tamanyo, TipoPan tipoPan)
+        {
+            Id = id;
+            Nombre = nombre;
+            PVP = pVP;
+            Stock = stock;
+            Tamanyo = tamanyo;
+            ResenyaBocadillo = new List<ResenyaBocadillo>();
+            CompraBocadillo = new List<CompraBocadillo>();
+            this.TipoPan = tipoPan;
+        }
+
         public Bocadillo(string nombre,float pVP, int stock, Tamanyo tamanyo)
         {
             Nombre = nombre;
