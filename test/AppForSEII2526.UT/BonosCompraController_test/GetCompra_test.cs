c29413c53400053a609e525ba78ae82a27dc3d1b
DANIEL MARTÍNEZ BAUTISTA <daniel.martinez28@alu.uclm.es>
2025-11-05 16:31:04 +0100

    Base de prueba de GetCompra

---
+++ test/AppForSEII2526.UT/BonosCompraController_test/GetCompra_test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForSEII2526.API.Controllers;
+using AppForSEII2526.API.DTOs;
+
+namespace AppForSEII2526.UT.BonosCompraController_test
+{
+    public class GetCompra_test
+    {
+        GetCompra_test() 
+        { 
+            
+        }
+    }
+}
