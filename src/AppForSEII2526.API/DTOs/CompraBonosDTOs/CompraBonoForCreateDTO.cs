8fec51d77a4b5e0332de22756a66dfc711871e78
DANIEL MARTÍNEZ BAUTISTA <daniel.martinez28@alu.uclm.es>
2025-11-05 17:02:31 +0100

    Arreglado falta de control de datos requeridos en CreateCompra

--- src/AppForSEII2526.API/DTOs/CompraBonosDTOs/CompraBonoForCreateDTO.cs
+++ src/AppForSEII2526.API/DTOs/CompraBonosDTOs/CompraBonoForCreateDTO.cs
@@ -17,12 +17,12 @@ namespace AppForSEII2526.API.DTOs.CompraBonosDTOs
             this.compraItems = compraItems;
         }
 
-        [Required]
+        //[Required]
         public string nombreCliente {  get; set; }
-        [Required]
+        //[Required]
         public string apellido1 { get; set; }
         public string? apellido2 { get; set; }
-        [Required]
+        //[Required]
         public MetodoPago metodoPago { get; set; }
         public List<CompraBonoItemDTO> compraItems { get; set; }
 
