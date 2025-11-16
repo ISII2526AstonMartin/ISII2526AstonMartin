0b2c008b2d8af2d564baabdbfdb1bf0c7430773a
RAFAEL MARTINEZ MUÑOZ <Rafael.Martinez18@alu.uclm.es>
2025-11-16 14:51:22 +0100

    Creación de pruebas unitarias del Details y comprobación de las mismas.

---
+++ dbo.Productos.data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[Productos] ON
+INSERT INTO [dbo].[Productos] ([ProductoID], [Nombre], [PVP], [Stock], [Tipo_ProductoProductoID]) VALUES (4, N'Camiseta', 8, 48, 1)
+INSERT INTO [dbo].[Productos] ([ProductoID], [Nombre], [PVP], [Stock], [Tipo_ProductoProductoID]) VALUES (5, N'Gorra', 4, 10, 2)
+INSERT INTO [dbo].[Productos] ([ProductoID], [Nombre], [PVP], [Stock], [Tipo_ProductoProductoID]) VALUES (6, N'Boligrafo', 2, 48, 3)
+SET IDENTITY_INSERT [dbo].[Productos] OFF
---
+++ dbo.TiposProductos.data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[TiposProductos] ON
+INSERT INTO [dbo].[TiposProductos] ([ProductoID], [Nombre]) VALUES (1, N'Camiseta')
+INSERT INTO [dbo].[TiposProductos] ([ProductoID], [Nombre]) VALUES (2, N'Gorra')
+INSERT INTO [dbo].[TiposProductos] ([ProductoID], [Nombre]) VALUES (3, N'Boligrafo')
+SET IDENTITY_INSERT [dbo].[TiposProductos] OFF
