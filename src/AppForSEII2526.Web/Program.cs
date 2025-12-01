5617f71869c17788ce10ae7d6c79fb834ef62356
Angel Lopez Hortelano <Angel.Lopez30@alu.uclm.es>
2025-12-01 19:26:57 +0100

    124 crear conexion a la api resenya (#152)
    * modificado .web
    
    * Configurada la conexion con la API

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using AppForSEII2526.Web.Components;
 using AppForSEII2526.Web.Components.Account;
 using AppForSEII2526.Web.Data;
+using AppForSEII2526.Web.API;
+using ApplicationUser = AppForSEII2526.Web.Data.ApplicationUser;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,6 +36,11 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+string? URI2API = builder.Configuration.GetValue(typeof(string), "AppForSEII2526_API") as string;  
+
+builder.Services.AddScoped<AppForSEII2526APIClient>(sp => new AppForSEII2526APIClient(URI2API, new HttpClient()));
+//AppForSEII2526APIClient
 
 var app = builder.Build();
 
