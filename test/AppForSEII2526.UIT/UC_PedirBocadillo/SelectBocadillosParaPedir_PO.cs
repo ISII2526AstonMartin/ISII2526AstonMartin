cf2c882ca00b08630085f828c11a08bfe4716290
ANTONIO GARCÍA DE LA REINA AGUILAR <Antonio.Garcia47@alu.uclm.es>
2025-12-10 20:11:41 +0100

    Avanzado las pruebas UIT del Select

---
+++ test/AppForSEII2526.UIT/UC_PedirBocadillo/SelectBocadillosParaPedir_PO.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+namespace AppForSEII2526.UIT.UC_Rental
+{
+    public class SelectBocadillosParaPedir_PO : PageObject
+    {
+        By inputTitle = By.Id("tamanyo");
+        By inputTipoPan = By.Id("selectTipoPan");
+        By buttonSearchBocadillos = By.Id("searchBocadillos");
+        By tableOfBocadillos = By.Id("TableOfBocadillos");
+        By errorShownBy = By.Id("ErrorsShown");
+        By buttonComprarBocadillos = By.Id("purchaseMovieButton");
+        public SelectBocadillosParaPedir_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+        public void SearchBocadillos(string tamanyo, string tipoPan)
+        {
+            //wait for the webelement to be clickable
+            WaitForBeingClickable(inputTitle);
+            _driver.FindElement(inputTitle).SendKeys(tamanyo);
+           
+
+
+            if (tipoPan == "") tipoPan = "All";
+            SelectElement selectElement = new SelectElement(_driver.FindElement(inputTipoPan));
+            selectElement.SelectByText(tipoPan);
+
+
+            _driver.FindElement(buttonSearchBocadillos).Click();
+        }
+
+
+        public bool CheckListOfBocadillos(List<string[]> expectedBocadillos)
+        {
+
+            return CheckBodyTable(expectedBocadillos, tableOfBocadillos);
+        }
+
+
+        public void AddBocadilloParaComprar(string nombreBocadillo)
+        {
+            WaitForBeingClickable(By.Id("movieToRent_" + nombreBocadillo));
+
+            _driver.FindElement(By.Id("movieToRent_" + nombreBocadillo)).Click();
+        }
+
+        public void RemoveBocadilloParaComprar(string nombreBocadillo)
+        {
+            WaitForBeingClickable(By.Id("removeMovie_" + nombreBocadillo));
+            _driver.FindElement(By.Id("removeMovie_" + nombreBocadillo)).Click();
+        }
+
+        public bool CompraNotAvailable()
+        {
+            //the button is not Displayed=hidden
+
+            return _driver.FindElement(buttonComprarBocadillos).Displayed == false;
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/UC_PedirBocadillo/UC_PedirBocadillo_UIT.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForMovies.UIT.Shared;
+using AppForSEII2526.UIT.Shared;
+
+namespace AppForSEII2526.UIT.UC_Rental
+{
+    public class UC_PedirBocadillo_UIT : UC_UIT
+    {
+        private SelectBocadillosParaPedir_PO selectBocadillosParaPedir_PO;
+        private const int bocadilloID = 1;
+        private const string nombreBocadillo1 = "BaconQueso";
+        private const string tipoPan1 = "Normal";
+        private const string precio1 = "3";
+        private const string tamanyo1 = "Normal";
+
+        private const string nombreBocadillo2 = "Serrano";
+        private const string tipoPan2 = "Integral";
+        private const string precio2 = "5";
+        private const string tamanyo2 = "Normal";
+
+
+
+        public UC_PedirBocadillo_UIT(ITestOutputHelper output) : base(output)
+        {
+            selectBocadillosParaPedir_PO = new SelectBocadillosParaPedir_PO(_driver, _output);
+
+
+        }
+
+
+        private void InitialStepsForCompraBocadillos()
+        {
+            
+            
+            selectBocadillosParaPedir_PO.WaitForBeingVisible(By.Id("CreateCompra"));
+            
+            _driver.FindElement(By.Id("CreateCompra")).Click();
+        }
+
+
+
+
+        [Theory]
+        [InlineData(nombreBocadillo1, tipoPan1, precio1, tamanyo1, "Normal", "")]
+        [InlineData(nombreBocadillo2, tipoPan2, precio2, tamanyo2, "", "Normal")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC2_AF1_UC2_4_5_6_filtering(string nombreBocadillo, string tipoPan, string precio, string tamanyo,
+            string filterTamanyo, string filterTipoPan)
+        {
+            //Arrange
+             InitialStepsForCompraBocadillos();
+             var expectedBocadillos = new List<string[]> { new string[] { nombreBocadillo, tipoPan,precio, tamanyo }, };
+
+            //Act
+
+            selectBocadillosParaPedir_PO.SearchBocadillos(filterTamanyo, filterTipoPan);
+
+            //Assert
+
+            Assert.True(selectBocadillosParaPedir_PO.CheckListOfBocadillos(expectedBocadillos));
+
+        }
+
+
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+
+        public void UC2_AF1_UC2_11_CompraNotAvailable()
+        {
+            //Arrange
+            InitialStepsForCompraBocadillos();
+            //Act
+            selectBocadillosParaPedir_PO.AddBocadilloParaComprar(nombreBocadillo1);
+            selectBocadillosParaPedir_PO.RemoveBocadilloParaComprar(nombreBocadillo1);
+
+            //Assert
+
+            Assert.True(selectBocadillosParaPedir_PO.CompraNotAvailable());
+
+        }
+
+
+
+
+
+
+    }
+
+
+    }
+
