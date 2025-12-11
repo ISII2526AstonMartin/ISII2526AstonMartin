157ca391d61e4296c273fc70f9d07a06a7b5b009
DANIEL MARTÍNEZ BAUTISTA <daniel.martinez28@alu.uclm.es>
2025-12-11 22:28:35 +0100

    terminadas pruebas de select

---
+++ test/AppForSEII2526.UIT/UC_CompraBonos/SelectBonosParaComprar_PO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace AppForSEII2526.UIT.UC_CompraBonos
+{
+    public class SelectBonosParaComprar_PO:PageObject
+    {
+        By inputnombre = By.Id("inputTitle");
+        By inputTipo = By.Id("selectTipoBocadillo");
+        By compraButton = By.Id("searchBonos");
+        By tablaofBonos = By.Id("TableOfBonos");
+
+        public SelectBonosParaComprar_PO(IWebDriver driver, ITestOutputHelper output) :base(driver, output)
+        {
+        }
+
+        public void BuscarBonos(string nombre, string tipo)
+        {
+            WaitForBeingClickable(inputnombre);
+            _driver.FindElement(inputnombre).SendKeys(nombre);
+            if (tipo == "") tipo = "Todos";
+            WaitForBeingClickable(inputTipo);
+            SelectElement se = new SelectElement(_driver.FindElement(inputTipo));
+            se.SelectByText(tipo);
+            _driver.FindElement(compraButton).Click();
+        }
+        
+        public bool CheckListOfBonos(List<string[]> expectedBonos)
+        {
+            return CheckBodyTable(expectedBonos, tablaofBonos);
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/UC_CompraBonos/UC_CompraBonos_UIT.cs
@@ -0,0 +1,89 @@
+﻿using AppForMovies.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_CompraBonos
+{
+    public class UC_CompraBonos_UIT : UC_UIT
+    {
+        private SelectBonosParaComprar_PO selectbonos_PO;
+        private const int bonoid = 1;
+        private const string nombre1 = "Bono1";
+        private const string tipo1 = "Vegano";
+        private const string bonoprice1 = "15";
+        private const string nbocadillos1 = "6";
+
+        public UC_CompraBonos_UIT(ITestOutputHelper output) : base(output)
+        {
+            
+            selectbonos_PO = new SelectBonosParaComprar_PO(_driver, _output);
+        }
+        private void InitialStepsForComprarBonos()
+        {
+            selectbonos_PO.WaitForBeingVisible(By.Id("CreateCompraBonos"));
+            _driver.FindElement(By.Id("CreateCompraBonos")).Click();
+        }
+        [Theory]
+        [InlineData(nombre1, tipo1, bonoprice1, nbocadillos1, "Bono1", "")]
+        [InlineData(nombre1, tipo1, bonoprice1, nbocadillos1, "", "Vegano")]
+        [InlineData(nombre1, tipo1, bonoprice1, nbocadillos1, "Bono1", "Vegano")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void CuBonos_AF1_2_3_4_5_FILTROS(string nombrebono, string tipoboca, string precio, string num, string filtronombre, string filtrotipo)
+        {
+            InitialStepsForComprarBonos();
+            var expectedBonos = new List<string[]>
+            {
+                new string[]
+                {
+                    nombrebono,tipoboca, num, precio
+                }
+            };
+
+            selectbonos_PO.BuscarBonos(filtronombre, filtrotipo);
+
+            Assert.True(selectbonos_PO.CheckListOfBonos(expectedBonos));
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void CuBonos_AF1_2_3_4_5_NOFILTROS()
+        {
+            InitialStepsForComprarBonos();
+            var expectedBonos = new List<string[]>
+            {
+                new string[]
+                {
+                    "Bono1", "Vegano", "6", "15"
+                },
+                new string[]
+                {
+                    "Bono2", "Vegetariano", "2","11"
+                },
+                new string[]
+                {
+                    "Bono3", "Sin Gluten", "6","10"
+                },
+
+            };
+
+            selectbonos_PO.BuscarBonos("", "");
+
+            Assert.True(selectbonos_PO.CheckListOfBonos(expectedBonos));
+        }
+        [Theory]
+        [InlineData("BonoNoExiste", "")]
+        [InlineData("", "TipoNoExiste")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void CuBonos_AF0(string filtronombre, string filtrotipo)
+        {
+            InitialStepsForComprarBonos();
+            var expectedBonos = new List< string[] >{ };
+            Assert.False(selectbonos_PO.CheckListOfBonos(expectedBonos));
+        }
+    }
+}
