468638cf02a32336e31d568126f8cb4fcdb7a6ea
ANTONIO GARCÍA DE LA REINA AGUILAR <Antonio.Garcia47@alu.uclm.es>
2025-12-12 18:40:20 +0100

    Creada la clase DetailPedido_PO

---
+++ test/AppForSEII2526.UIT/UC_PedirBocadillo/DetailPedido_PO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_PedirBocadillo
+{
+    internal class DetailPedido_PO :PageObject
+    {
+        public DetailPedido_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+        
+    }
+}
+
