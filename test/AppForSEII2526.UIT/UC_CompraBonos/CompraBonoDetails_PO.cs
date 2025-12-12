3cefda658641de3ef2b94ec8e03c6ee48abafa89
DANIEL MARTÍNEZ BAUTISTA <daniel.martinez28@alu.uclm.es>
2025-12-12 21:32:12 +0100

    terminadas pruebas de details y subido documento de casos de prueba de CU3 Comprar Bonos

--- test/AppForSEII2526.UIT/UC_CompraBonos/CompraBonoDetails_PO.cs
+++ test/AppForSEII2526.UIT/UC_CompraBonos/CompraBonoDetails_PO.cs
@@ -8,8 +8,41 @@ namespace AppForSEII2526.UIT.UC_CompraBonos
 {
     public class CompraBonoDetails_PO : PageObject
     {
+        By tablaofBonos = By.Id("BonosCompradosTable");
+        By namesurname = By.Id("NameSurname");
+        By pago = By.Id("PaymentMethod");
+        By fechahtml = By.Id("RentalDate");
+        By total = By.Id("TotalPrice");
+
         public CompraBonoDetails_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
+        }
+        public bool CheckListOfBonos(List<string[]> expectedBonos)
+        {
+            
+            WaitForBeingVisible(tablaofBonos);
+            return CheckBodyTable(expectedBonos, tablaofBonos);
+            
+        }
+        public bool CheckListOfDatos(string nombreyapellidos, string fecha, string metodopago, string precio)
+        {
+            WaitForBeingVisible(namesurname);
+            IWebElement containername = _driver.FindElement(namesurname);
+            string actualname = containername.Text;
+            
+            WaitForBeingVisible(pago);
+            IWebElement containerpago = _driver.FindElement(pago);
+            string actualpago = containerpago.Text;
+
+            WaitForBeingVisible(fechahtml);
+            IWebElement containerfecha = _driver.FindElement(fechahtml);
+            string actualfecha = containerfecha.Text;
+
+            WaitForBeingVisible(total);
+            IWebElement containertotal = _driver.FindElement(total);
+            string actualtotal = containertotal.Text;
+            
+            return actualname.Contains(nombreyapellidos)&&actualpago.Contains(metodopago)&&actualfecha.Contains(fecha)&&actualtotal.Contains(precio);
         }
     }
 }
